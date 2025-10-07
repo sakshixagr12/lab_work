a9568e89cafa3f2bf8c09d457285afab6fb530a9
Sakshi Agrahari <agraharisakku@gmail.com>
2025-10-07 12:35:36 +0530

    insertion sorting

---
+++ main (11).c
@@ -0,0 +1,35 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int n ;
+    printf("enter the size of array\n");
+    scanf("%d",&n);
+    
+    int arr[n];
+    for(int i =0;i<n;i++){
+        printf("arr[%d] : ",i);
+        scanf("%d",&arr[i]);
+    }
+    for(int i =0 ;i<n;i++){
+        printf("%d\t",arr[i]);
+    }
+    //sorting
+    int temp;
+
+    for(int i=1;i<n;i++){
+        int current = arr [i];
+        int j = i-1;
+        while(arr[j]>current && j>=0) {
+            arr[j+1]=arr[j];
+            j--;
+        }
+        arr[j+1]=current;
+    }
+    printf("\nafter swapping\n");
+    for(int i=0 ;i < n ; i++){
+        printf("%d\t",arr[i]);
+    }
+    return 0;
+}
